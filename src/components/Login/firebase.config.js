3d99503d3bba562390060e8871063b0444968b78
alimmondal <italimbd@gmail.com>
2021-04-07 21:51:16 -0700

    client

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAp43ol48h5a3HqjM03msAS3XzECpJ4rNA",
+  authDomain: "full-stack-client-34a29.firebaseapp.com",
+  projectId: "full-stack-client-34a29",
+  storageBucket: "full-stack-client-34a29.appspot.com",
+  messagingSenderId: "495197277990",
+  appId: "1:495197277990:web:8231e06882c9a9aafcfbe7"
+};
+  
+  export default firebaseConfig;
